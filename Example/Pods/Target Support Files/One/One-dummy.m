e714691993bd3633a7f6954046915daa7766fa8b
Kostadina Geceska <kostadinag@nextsense.com>
2024-06-20 11:12:04 +0200

    second commit

---
+++ Example/Pods/Target Support Files/One/One-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_One : NSObject
+@end
+@implementation PodsDummy_One
+@end
---
+++ One/Classes/LblExtension.swift
@@ -0,0 +1,33 @@
+
+import Foundation
+import UIKit
+
+
+public extension UILabel {
+    
+   public func underlinedLabel() {
+        guard let text = text else { return }
+        let textRange = NSRange(location: 0, length: text.count)
+        let attributedText = NSMutableAttributedString(string: text)
+        attributedText.addAttribute(.underlineStyle,
+                                    value: NSUnderlineStyle.styleSingle.rawValue,
+                                    range: textRange)
+        self.attributedText = attributedText
+    }
+    
+    func partTextChangedStyle(fullText: String,
+                          changeText: String,
+                          color: UIColor = .black,
+                          font: UIFont = .systemFont(ofSize: 14)) {
+        let strNumber: NSString = fullText as NSString
+        let range = (strNumber).range(of: changeText, options: .backwards)
+        let attribute = NSMutableAttributedString.init(string: fullText)
+        attribute.addAttribute(NSAttributedString.Key.foregroundColor, value: color, range: range)
+        attribute.addAttribute(NSAttributedString.Key.font, value: font , range: range)
+
+        self.attributedText = attribute
+        self.setNeedsDisplay()
+        self.layoutIfNeeded()
+    }
+    
+}
